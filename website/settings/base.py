28aef5c5eb9918b1b497df91bf8af85b9a626f72
joelcm20 <132691926+joelcm20@users.noreply.github.com>
2023-06-29 20:24:30 -0300

    app usuario agregado

--- website/settings/base.py
+++ website/settings/base.py
@@ -16,6 +16,8 @@ from os import path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+AUTH_USER_MODEL = 'usuario.Usuario'
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -31,6 +33,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.usuario'
 ]
 
 MIDDLEWARE = [
