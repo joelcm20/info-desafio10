1ddd2970c49c45696deca099ce7eec68ab710fb6
aguerima <imanolaguer1@gmail.com>
2023-07-28 22:57:44 -0300

    copia seguridad

--- apps/comment/forms.py
+++ apps/comment/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import Comentario
 
 
-class CommentForm(forms.ModelForm):
+class CommentForm(forms.ModelForm): 
     class Meta:
         model = Comentario
         fields = ["texto"]
