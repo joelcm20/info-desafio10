9a4f55fe8ae3281dc7f5b2ec51f0235857a9d4e6
aguerima <imanolaguer1@gmail.com>
2023-07-31 18:12:24 -0300

    copia precaucion

--- blog/settings/local.py
+++ blog/settings/local.py
@@ -13,12 +13,18 @@ ALLOWED_HOSTS = []
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": 'django.db.backends.mysql',
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "NAME": os.getenv("DB_NAME"),
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog_info',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
+
+print("DB_HOST:", os.getenv("DB_HOST"))
+print("DB_PORT:", os.getenv("DB_PORT"))
+print("DB_USER:", os.getenv("DB_USER"))
+print("DB_PASSWORD:", os.getenv("DB_PASSWORD"))
+print("DB_NAME:", os.getenv("DB_NAME"))
